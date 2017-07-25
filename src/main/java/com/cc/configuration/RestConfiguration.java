9ecba5dc45e27d85a9de9f10c7d258fe23d88c59
cuicong <cuicong@12lian.com>
2017-07-25 18:30:00 +0800

    Haha

--- src/main/java/com/cc/configuration/RestConfiguration.java
+++ src/main/java/com/cc/configuration/RestConfiguration.java
@@ -1,24 +1,13 @@
 package com.cc.configuration;
 
-import com.cc.configuration.interceptor.LoggingRequestInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
-import org.springframework.boot.autoconfigure.web.WebClientAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.client.ClientHttpRequestInterceptor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.client.SimpleClientHttpRequestFactory;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.web.client.RestOperations;
+import org.springframework.web.client.AsyncRestTemplate;
 import org.springframework.web.client.RestTemplate;
-
-import java.nio.charset.Charset;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Iterator;
-import java.util.List;
 
 /**
  * Created by cong on 2017/7/24.
@@ -29,28 +18,19 @@ public class RestConfiguration {
     private String steamKey;
 
 
-
-
     @Bean
-    @ConditionalOnMissingBean({RestOperations.class, RestTemplate.class})
-    public RestOperations restOperations() {
+    public RestTemplate restOperations() {
         SimpleClientHttpRequestFactory requestFactory = new SimpleClientHttpRequestFactory();
         requestFactory.setReadTimeout(5000);
         requestFactory.setConnectTimeout(5000);
 
         RestTemplate restTemplate = new RestTemplate(requestFactory);
+        HttpHeaders headers = new HttpHeaders();
+        MediaType type = MediaType.parseMediaType("application/json; charset=UTF-8");
+        headers.setContentType(type);
+        headers.add("Accept", MediaType.APPLICATION_JSON.toString());
 
-        // 使用 utf-8 编码集的 conver 替换默认的 conver（默认的 string conver 的编码集为 "ISO-8859-1"）
-        List<HttpMessageConverter<?>> messageConverters = restTemplate.getMessageConverters();
-        Iterator<HttpMessageConverter<?>> iterator = messageConverters.iterator();
-        while (iterator.hasNext()) {
-            HttpMessageConverter<?> converter = iterator.next();
-            if (converter instanceof StringHttpMessageConverter) {
-                iterator.remove();
-            }
-        }
-        messageConverters.add(new StringHttpMessageConverter(Charset.forName("UTF-8")));
-        restTemplate.setInterceptors(Arrays.asList(new LoggingRequestInterceptor()));
+        // restTemplate.setInterceptors(Arrays.asList(new LoggingRequestInterceptor()));
         return restTemplate;
     }
 }
