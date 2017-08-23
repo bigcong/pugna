9c9f8b73c242303ab2c654fb417358be352fec2e
cuicong <cuicong@12lian.com>
2017-08-23 16:10:13 +0800

    时间格式化的问题

--- src/main/java/com/cc/util/DateUtil.java
+++ src/main/java/com/cc/util/DateUtil.java
@@ -8,7 +8,7 @@ import java.util.Date;
  * Created by bigcong on 15/12/3.
  */
 public class DateUtil {
-    public static DateFormat format=new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+    public static DateFormat format=new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     public static String nowDate(){
         return format.format(new Date());
     }
